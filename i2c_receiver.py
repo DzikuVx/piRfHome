2374e88ef24c2fdc9c3572567ea3ae9e5aa1b103
DzikuVx <pspychalski@gmail.com>
2015-03-18 22:47:01 +0100

    RF receiver over I2C bus

--- i2c_receiver.py
+++ i2c_receiver.py
@@ -9,8 +9,25 @@ DEVICE_ADDR = 0x43
 
 bus = smbus.SMBus(BUS_NUMBER)
 
+while 1 == 1:
+
+	msgLen = bus.read_byte(DEVICE_ADDR)
+
+	if msgLen > 0:
+
+		out = []
+
+		for i in range(0, msgLen):
+			out.append(bus.read_byte(DEVICE_ADDR))
+
+		print(out)
+
+	time.sleep(0.2)
+
+'''
 for i in range(0, 9):
 
 	val = bus.read_byte(DEVICE_ADDR)
 
 	print str.join("", ("%02x" % val))
+'''
